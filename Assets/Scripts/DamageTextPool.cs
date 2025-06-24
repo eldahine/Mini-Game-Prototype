e9bed71f7b3e5e778f21f47e21852009cf39beef
Ahmed Shaheen <110909069+eldahine@users.noreply.github.com>
2025-06-24 20:20:21 +0300

    Add damage point object pool

---
+++ Assets/Scripts/DamageTextPool.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTextPool : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> digitSprites;
+    
+    public static DamageTextPool SharedInstance;
+    [SerializeField] private List<GameObject> pooledObjects;
+    [SerializeField] private GameObject objectToPool;
+    [SerializeField] private int amountToPool;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < amountToPool; i++)
+        {
+            tmp = Instantiate(objectToPool);
+            tmp.SetActive(false);
+            pooledObjects.Add(tmp);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+
+    public void Spawn(int damageAmount, Vector2 position)
+    {
+        GameObject digit = GetPooledObject();
+        if (digit != null)
+        {
+            digit.transform.position = position;
+            digit.GetComponent<SpriteRenderer>().sprite = digitSprites[damageAmount];
+            digit.SetActive(true);
+        }
+    }
+
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,19 +3,42 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [Header("Health Variables")]
-    [SerializeField] private int max_health = 100;
+    [SerializeField] private int maxHealth = 100;
     [SerializeField] private int health = 100;
     
     void Start()
     {
-        health = max_health;    
+        health = maxHealth;    
     }
 
-    public void Damage(int damageAmount) {
+    public void Damage(int damageAmount)
+    {
         health -= damageAmount;
 
-        if (health <= 0) {
+        if (health <= 0)
+        {
             Die();
+        }
+        else
+        {
+            while (damageAmount > 9)
+            {
+                var amount = Random.Range(3, 8);
+                damageAmount -= amount;
+
+                var textPosition = new Vector2(transform.position.x, transform.position.y);
+                textPosition += transform.localScale * (Random.insideUnitCircle * 0.5f);
+
+                DamageTextPool.SharedInstance.Spawn(amount, textPosition);
+            }
+
+            if (damageAmount > 0)
+            {
+                var textPosition = new Vector2(transform.position.x, transform.position.y);
+                textPosition += transform.localScale * (Random.insideUnitCircle * 0.5f);
+
+                DamageTextPool.SharedInstance.Spawn(damageAmount, textPosition);
+            }
         }
     }
 
